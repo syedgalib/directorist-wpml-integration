30f74311996d58340c6a91551206b3959289e287
Syed Galib <syedgalibahmed@gmail.com>
2022-05-23 09:34:38 +0600

    refactored

--- app/Controller/Hook/Directory_Builder_Actions.php
+++ app/Controller/Hook/Directory_Builder_Actions.php
@@ -14,8 +14,8 @@ class Directory_Builder_Actions {
      * @return void
      */
     public function __construct() {
-        add_action( 'directorist_before_set_default_directory_type', [ $this, 'before_set_default_directory_type' ], 20, 2 );
-        add_action( 'directorist_after_set_default_directory_type', [ $this, 'after_set_default_directory_type' ], 20, 3 );
+        add_action( 'directorist_before_set_default_directory_type', [ $this, 'before_set_default_directory_type' ], 20, 1 );
+        add_action( 'directorist_after_set_default_directory_type', [ $this, 'after_set_default_directory_type' ], 20, 1 );
     }
 
     /**
@@ -24,7 +24,11 @@ class Directory_Builder_Actions {
      * @param int $directory_type_id
      * @return void
      */
-    public function before_set_default_directory_type( $directory_type_id = 0, $current_language ) {
+    public function before_set_default_directory_type( $directory_type_id = 0 ) {
+
+        $current_language = apply_filters( 'wpml_current_language', NULL );
+
+        set_transient( 'directorist_wpml_integration:current_language', $current_language );
 
         do_action( 'wpml_switch_language', 'all' );
 
@@ -34,14 +38,18 @@ class Directory_Builder_Actions {
      * After set default directory type
      * 
      * @param int $directory_type_id
-     * @param array $all_directory_types
-     * @param string $current_language
      * 
      * @return void
      */
-    public function after_set_default_directory_type( $directory_type_id = 0, $all_directory_types, $current_language ) {
-        
+    public function after_set_default_directory_type( $directory_type_id = 0 ) {
+
+        $current_language  = apply_filters( 'wpml_current_language', NULL );
+        $previous_language = get_transient( 'directorist_wpml_integration:current_language' );
+        $current_language  = ( ! empty( $previous_language ) ) ? $previous_language : $current_language;
+
         do_action( 'wpml_switch_language', $current_language );
+
+        delete_transient( 'directorist_wpml_integration:current_language' );
 
         $element_type   = apply_filters( 'wpml_element_type', ATBDP_DIRECTORY_TYPE );
         $translation_id = apply_filters( 'wpml_element_trid', NULL, $directory_type_id, $element_type );
