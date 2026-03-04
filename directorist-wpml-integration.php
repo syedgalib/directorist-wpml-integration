6f80fd254c0d0d85964e190bc5c111a58ce4e3a1
Yasir Arafat <148990700+Arafat-plugins@users.noreply.github.com>
2026-03-04 14:59:35 +0600

    version uiipgrated

--- directorist-wpml-integration.php
+++ directorist-wpml-integration.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/sovware/directorist-wpml-integration
  * Description: WPML integration plugin for Directorist.
  * Requires Plugins: directorist
- * Version: 2.2.1
+ * Version: 2.2.2
  * Requires at least: 6.0
  * Requires PHP: 7.4
  * Tested up to: 6.9
